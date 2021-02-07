e8ad4a07040694f21bf801be786e3892adbd9e94
Asbjørn Engås <aen@dips.no>
2021-02-08 00:02:56 +0100

    Tarifa

---
+++ Algoritmer/Tarifas/Tarifa.cs
@@ -0,0 +1,19 @@
+﻿namespace Algoritmer.Tarifas
+{
+    public static class Tarifa
+    {
+        public static void Calculate(IConsole console)
+        {
+            int X = int.Parse(console.ReadLine());
+            int N = int.Parse(console.ReadLine());
+
+            int notUsed = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                notUsed += X - int.Parse(console.ReadLine());
+            }
+            
+            console.WriteLine((notUsed + X).ToString());
+        }
+    }
+}
---
+++ Test.Algoritmer/Tarifas/TarifaTests.cs
@@ -0,0 +1,45 @@
+﻿using Algoritmer.Tarifas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Algoritmer.Tarifas
+{
+    [TestClass]
+    public class TarifaTests : TestBase
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+            string[] input = { "10", "3", "4", "6", "2" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "28" };
+
+            Tarifa.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TestTwo()
+        {
+            string[] input = { "10", "3", "10", "2", "12" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "16" };
+
+            Tarifa.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TestThree()
+        {
+            string[] input = { "15", "3", "15", "10", "20" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "15" };
+
+            Tarifa.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+    }
+}
