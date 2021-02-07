00adddc4a8f0bfc6961775a5cb8605044af8ea3f
Asbjørn Engås <aen@dips.no>
2021-02-07 23:45:03 +0100

    Zamka

---
+++ Algoritmer/Zamkas/Zamka.cs
@@ -0,0 +1,47 @@
+﻿namespace Algoritmer.Zamkas
+{
+    public static class Zamka
+    {
+        public static void Calculate(IConsole console)
+        {
+            int L = int.Parse(console.ReadLine());
+            int D = int.Parse(console.ReadLine());
+            int X = int.Parse(console.ReadLine());
+
+            int N = 0;
+            for (int i = L; i <= D; i++)
+            {
+                if (Across(i) == X)
+                {
+                    N = i;
+                    break;
+                }
+            }
+
+            int M = 0;
+            for (int j = D; j >= L; j--)
+            {
+                if (Across(j) == X)
+                {
+                    M = j;
+                    break;
+                }
+            }
+            
+            console.WriteLine(N.ToString());
+            console.WriteLine(M.ToString());
+        }
+
+        private static int Across(int n)
+        {
+            int sum = 0;
+            while (n != 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Test.Algoritmer/Zamkas/ZamkaTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algoritmer.Zamkas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Algoritmer.Zamkas
+{
+    [TestClass]
+    public class ZamkaTests : TestBase
+    {
+        [TestMethod]
+        public void TestOne()
+        {
+            string[] input = { "1", "100", "4" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "4", "40" };
+
+            Zamka.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TestTwo()
+        {
+            string[] input = { "100", "500", "12" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "129", "480" };
+
+            Zamka.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TestThree()
+        {
+            string[] input = { "1", "10000", "1" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "1", "10000" };
+
+            Zamka.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+    }
+}
