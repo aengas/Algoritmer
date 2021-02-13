64f1a23064f68c6491a7bffa2311577bdffeac46
Asbjørn Engås <aen@dips.no>
2021-02-13 19:21:21 +0100

    QALY

---
+++ Algoritmer/QALYs/Qaly.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Algoritmer.QALYs
+{
+    public static class Qaly
+    {
+        public static void Calculate(IConsole console)
+        {
+            double sum = 0.0;
+            int numberOfInputs = int.Parse(console.ReadLine());
+            for (int i = 1; i <= numberOfInputs; i++)
+            {
+                string[] split = console.ReadLine().Split(' ');
+                double qualityOfLife = double.Parse(split[0], CultureInfo.InvariantCulture);
+                double period = double.Parse(split[1], CultureInfo.InvariantCulture);
+
+                sum += qualityOfLife * period;
+            }
+
+            console.WriteLine(sum.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Test.Algoritmer/QALYs/QalyTests.cs
@@ -0,0 +1,45 @@
+﻿using Algoritmer.QALYs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Algoritmer.QALYs
+{
+    [TestClass]
+    public class QalyTests : TestBase
+    {
+        [TestMethod]
+        public void OneLine()
+        {
+            string[] input = { "1", "1.0 12.0" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "12.000" };
+
+            Qaly.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TwoLines()
+        {
+            string[] input = { "2", "1.0 3.0", "2.0 5.0" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "13.000" };
+
+            Qaly.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void FiveLines()
+        {
+            string[] input = { "5", "1.0 12.0", "0.7 5.2", "0.9 10.7", "0.5 20.4", "0.2 30.0" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "41.470" };
+
+            Qaly.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+    }
+}
