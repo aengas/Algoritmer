83f7ebe58ea3db322574e60acae6fbf910bf4262
Asbjørn Engås <aen@dips.no>
2021-02-27 17:09:31 +0100

    CourseScheduler

---
+++ Algoritmer/CourseSchedulers/CourseScheduler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algoritmer.CourseSchedulers
+{
+    public static class CourseScheduler
+    {
+        public static void Calculate(IConsole console)
+        {
+            int n = int.Parse(console.ReadLine());
+
+            var courses = new Dictionary<string, HashSet<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = console.ReadLine();
+                string course = line.Substring(line.LastIndexOf(' ') + 1);
+                
+                if (courses.TryGetValue(course, out HashSet<string> hashSet))
+                {
+                    hashSet.Add(line);
+                }
+                else
+                {
+                    courses.Add(course, new HashSet<string> { line });
+                    
+                }
+            }
+
+            foreach ((string key, HashSet<string> value) in courses.OrderBy(c => c.Key))
+            {
+                console.WriteLine(string.Join(' ', key, value.Count.ToString()));
+            }
+        }
+
+    }
+    
+    
+}
---
+++ Test.Algoritmer/CourseSchedulers/CourseSchedulerTests.cs
@@ -0,0 +1,57 @@
+﻿using Algoritmer.CourseSchedulers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Algoritmer.CourseSchedulers
+{
+    [TestClass]
+    public class CourseSchedulerTests : TestBase
+    {
+        [TestMethod]
+        public void OneStudent_OneCourse()
+        {
+            string[] input = { "1", "PINK TIE CS241" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "CS241 1" };
+
+            CourseScheduler.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TwoStudents_OneCourse()
+        {
+            string[] input = { "2", "PINK TIE CS241", "JOHN DOE CS241" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "CS241 2" };
+
+            CourseScheduler.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TwoStudents_TwoCourse()
+        {
+            string[] input = { "3", "PINK TIE CS241", "JOHN DOE CS241", "JOHN DOE CSS" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "CS241 2", "CSS 1" };
+
+            CourseScheduler.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TwoStudents_OneDuplicate_TwoCourse()
+        {
+            string[] input = { "4", "PINK TIE CS241", "JOHN DOE CS241", "JOHN DOE CSS", "JOHN DOE CSS" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "CS241 2", "CSS 1" };
+
+            CourseScheduler.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+    }
+}
