cae6047325119228a28cd1e047ecb3f443ecf774
Asbjørn Engås <aen@dips.no>
2021-03-20 18:55:19 +0100

    Happy Telephone

---
+++ Algoritmer/HappyTelephones/HappyTelephone.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Algoritmer.HappyTelephones
+{
+    public static class HappyTelephone
+    {
+        public static void Calculate(IConsole Console)
+        {
+            while (true)
+            {
+                string firstLine = Console.ReadLine();
+                int N = int.Parse(firstLine.Substring(0, firstLine.IndexOf(' ') + 1));
+                int M = int.Parse(firstLine.Substring(firstLine.IndexOf(' ') + 1));
+                if (N == 0 && M == 0)
+                {
+                    break;
+                }
+
+                var calls = new Interval[N];
+                for (int i = 0; i < N; i++)
+                {
+                    string line = Console.ReadLine();
+                    string[] lineSplit = line.Split(new[] { ' ' }, StringSplitOptions.None);
+                    int callStart = int.Parse(lineSplit[2]);
+                    int callDuration = int.Parse(lineSplit[3]);
+                    calls[i] = new Interval(callStart, callDuration);
+                }
+
+                for (int i = 0; i < M; i++)
+                {
+                    string intervalLine = Console.ReadLine();
+                    int intervalStart = int.Parse(intervalLine.Substring(0, intervalLine.IndexOf(' ') + 1));
+                    int intervalDuration = int.Parse(intervalLine.Substring(intervalLine.IndexOf(' ') + 1));
+                    var currentInterval = new Interval(intervalStart, intervalDuration);
+                    long sum = 0;
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (currentInterval.AreActive(calls[j]))
+                        {
+                            sum++;
+                        }
+                    }
+                    Console.WriteLine(sum.ToString());
+                }
+            }
+        }
+
+        public class Interval
+        {
+            public int Start { get; set; }
+            public int End { get; set; }
+            public Interval(int start, int duration)
+            {
+                Start = start;
+                End = start + duration;
+            }
+
+            public bool AreActive(Interval other)
+            {
+                return ((Start <= (other.Start)) && (other.Start < End))
+                       || ((Start < other.End) && (other.End <= End))
+                       || ((other.Start < Start) && (other.End > End));
+            }
+        }
+    }
+}
---
+++ Test.Algoritmer/HappyTelephones/HappyTelephoneTests.cs
@@ -0,0 +1,81 @@
+﻿using Algoritmer.HappyTelephones;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Algoritmer.HappyTelephones
+{
+    [TestClass]
+    public class HappyTelephoneTests : TestBase
+    {
+        [TestMethod]
+        public void OneCall_OneIntervalInCall()
+        {
+            string[] input = { "1 1", "1 2 0 10", "1 2", "0 0" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "1" };
+
+            HappyTelephone.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void OneCall_OneIntervalOnBeginningOfCall()
+        {
+            string[] input = { "1 1", "1 2 0 10", "0 2", "0 0" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "1" };
+
+            HappyTelephone.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void OneCall_OneIntervalOnEndOfCall()
+        {
+            string[] input = { "1 1", "1 2 0 10", "9 1", "0 0" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "1" };
+
+            HappyTelephone.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void OneCall_OneIntervalSpanningWholeOfCall()
+        {
+            string[] input = { "1 1", "1 2 0 10", "0 10", "0 0" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "1" };
+
+            HappyTelephone.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void TwoCalls_TwoIntervals_OneIntervalWithTwoCalls_OneIntervalWithOneCall()
+        {
+            string[] input = { "2 2", "3 4 2 5", "1 2 0 10", "0 6", "8 2", "0 0" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "2", "1" };
+
+            HappyTelephone.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void SampleInputOne()
+        {
+            string[] input = { "3 2", "3 4 2 5", "1 2 0 10", "6 5 5 8", "0 6", "8 2", "1 2", "8 9 0 10", "9 10", "10 1", "0 0"};
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "3", "2", "1", "0" };
+
+            HappyTelephone.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+    }
+}
