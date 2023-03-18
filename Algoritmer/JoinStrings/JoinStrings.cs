9b7bb2fa44ef2b2d9be5d407bddcbbb8ba7786c3
Asbjørn Engås <aen@dips.no>
2023-03-19 00:49:42 +0100

    JoinStrings

---
+++ Algoritmer/JoinStrings/JoinStrings.cs
@@ -0,0 +1,37 @@
+﻿namespace Algoritmer.JoinStrings
+{
+    public static class JoinStringsCalculator 
+    {
+        public static void Calculate(IConsole Console)
+        {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+            string[] strings = new string[numberOfStrings + 1];
+            for (int i = 1; i <= numberOfStrings; i++)
+            {
+                strings[i] = Console.ReadLine();
+            }
+
+            string remainingString = "";
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                string[] pair = line.Split(' ');
+                int a = int.Parse(pair[0]);
+                int b = int.Parse(pair[1]);
+                strings[a] += strings[b];
+                remainingString = strings[a];
+                strings[b] = "";
+            }
+
+            if (remainingString == "")
+            {
+                remainingString = strings[1];
+            }
+            Console.WriteLine(remainingString);
+        }
+    }
+}
---
+++ Test.Algoritmer/JoinStrings/JoinStringsTests.cs
@@ -0,0 +1,33 @@
+﻿using Algoritmer.JoinStrings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Algoritmer.JoinStrings
+{
+    [TestClass]
+    public class JoinStringsTests : TestBase
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            string[] input = { "4", "cute", "cat", "kattis", "is", "3 2", "4 1", "3 4" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "kattiscatiscute" };
+
+            JoinStringsCalculator.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            string[] input = { "3", "howis", "this", "practicalexam", "1 2", "1 3" };
+            var consoleFaker = new ConsoleFaker(input);
+            string[] expected = { "howisthispracticalexam" };
+
+            JoinStringsCalculator.Calculate(consoleFaker);
+
+            AssertOutput(expected, consoleFaker.Output);
+        }
+    }
+}
