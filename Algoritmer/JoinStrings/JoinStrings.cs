f4bb2adfaad2719dcbc0a8555b048d4dfe64ee1c
Asbjørn Engås <aen@dips.no>
2023-03-22 21:33:07 +0100

    små endringer i JoinStrings

--- Algoritmer/JoinStrings/JoinStrings.cs
+++ Algoritmer/JoinStrings/JoinStrings.cs
@@ -7,26 +7,25 @@ namespace Algoritmer.JoinStrings
         public static void Calculate(IConsole Console)
         {
             int numStrs = int.Parse(Console.ReadLine());
-            StringNode[] array = new StringNode[numStrs];
+            var array = new StringNode[numStrs];
 
             for (int i = 0; i < numStrs; i++)
             {
                 array[i] = new StringNode(Console.ReadLine());
             }
 
-            int idx = 0;
+            int a = 0;
             for (int j = 0; j < numStrs -1; j++)
             {
                 string input = Console.ReadLine();
                 int spaceIndex = input.IndexOf(' ');
-                int a = int.Parse(input.Substring(0, spaceIndex)) - 1;
+                a = int.Parse(input.Substring(0, spaceIndex)) - 1;
                 int b = int.Parse(input.Substring(spaceIndex + 1)) - 1;
                 array[a].Append(array[b]);
-                idx = a;
             }
 
-            StringBuilder strb = new StringBuilder();
-            for (StringNode n = array[idx]; n != null; n = n.Next)
+            var strb = new StringBuilder();
+            for (StringNode n = array[a]; n != null; n = n.Next)
             {
                 strb.Append(n.S);
              
